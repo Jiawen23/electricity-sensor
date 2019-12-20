bff6359241ab0fb3383cbfed3a0a6274403a8e6a
ZHU Jiawen <jiawen.zhu@yahoo.com>
2019-12-20 23:30:59 +0100

    Initial Commit

---
+++ electricity sensor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  electricity sensor
+//
+//  Created by 朱佳雯 on 20/12/2019.
+//  Copyright © 2019 朱佳雯. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
